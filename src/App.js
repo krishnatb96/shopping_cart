4e356a5be9a38cbe2bd88208ce0c1df59ab73ae7
krishnatb96 <120475705+krishnatb96@users.noreply.github.com>
2023-02-04 17:46:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Route,Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+
+<BrowserRouter>
+<Header />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="cart" element={<Cart />} />
+      </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
